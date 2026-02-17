33228a80fbeb09db180187bcba29531c48c81a4c
Aaron Coste <aaron031229@gmail.com>
2026-02-17 08:04:08 -0400

    feat: healh route

--- src/app.js
+++ src/app.js
@@ -3,7 +3,11 @@ import express from "express";
 const port = process.env.port || 3000;
 const app = express();
 
-app.get("/e", (req, res) => {
+app.get("/health", (req, res) => {
+    return res.json({ up: true, date: new Date().getDate().toString()})
+});
+
+app.get("/", (req, res) => {
     return res.json({ message: "Hola Mundo" })
 });
 
